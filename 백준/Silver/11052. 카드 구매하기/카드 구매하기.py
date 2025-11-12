33ad439b0e199fc3182d03ffcfac643a4513d5c8
hoho <hoyeonlove1004@naver.com>
2025-11-12 10:39:59 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -4,8 +4,8 @@ dp = [0 for _ in range(n+1)]
 max_value = 0
 
 for i in range(1,n+1):
+    best = 0
     for j in range(1,i+1):
-        max_value = max(cards[j-1] + dp[i-j], max_value)
-    dp[i] = max_value
-    max_value = dp[i]
+        best = max(cards[j-1] + dp[i-j], best)
+    dp[i] = best
 print(dp[n])
