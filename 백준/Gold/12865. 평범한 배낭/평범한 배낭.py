dadf017a0300e76e5ff560fef7bc48d7ad276884
Park Hoyeon <hoyeonlove1004@naver.com>
2026-01-11 12:17:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 3892 ms, Memory: 230376 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+bag = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    w,v = bag[i-1]
+    for j in range(1,k+1):
+        if j >= w:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[n][k])
