0ed45ad96b259acadba4bf1310e6ac291bab23fb
Park Hoyeon <hoyeonlove1004@naver.com>
2026-01-20 18:34:05 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 220 ms, Memory: 112184 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,26 @@
+while True:
+    word = input()
+    stack = []
+
+    if word == ".":
+        break
+
+    for i in word:
+        if i == '[' or i == '(':
+            stack.append(i)
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
