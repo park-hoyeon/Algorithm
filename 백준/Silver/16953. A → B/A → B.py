de4e53d9f8958b7ed44ab87d5d945896ff8bd8a3
Park Hoyeon <hoyeonlove1004@naver.com>
2026-01-24 00:29:02 +0900

    [Silver II] Title: A → B, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a,b = map(int, input().split())
+answer = 1
+
+while b > a:
+    if b % 10 == 1:
+        b //= 10
+        answer += 1
+    elif b % 2 == 0:
+        b //= 2
+        answer += 1
+    else:
+        print(-1)
+        break
+else:
+    print(answer if b == a else -1)
