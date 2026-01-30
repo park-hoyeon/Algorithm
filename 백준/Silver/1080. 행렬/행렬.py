30e03bd0f03fd8bdf4cac668ae6ed4ddfdbecb2e
Park Hoyeon <hoyeonlove1004@naver.com>
2026-01-31 00:58:47 +0900

    [Silver I] Title: 행렬, Time: 104 ms, Memory: 110836 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+a = [list(map(int, input().strip())) for _ in range(n)]
+b = [list(map(int, input().strip())) for _ in range(n)]
+
+def flip(x,y):
+    for i in range(x,x+3):
+        for j in range(y,y+3):
+            a[i][j] = 1 - a[i][j]
+
+count = 0
+for i in range(n-2):
+    for j in range(m-2):
+        if a[i][j] != b[i][j]:
+            flip(i,j)
+            count += 1
+
+if a==b:
+    print(count)
+else:
+    print(-1)
