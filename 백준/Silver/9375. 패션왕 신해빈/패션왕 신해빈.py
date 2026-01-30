cb507cd37211ea2afc3e370769c49788eb0f3838
Park Hoyeon <hoyeonlove1004@naver.com>
2026-01-31 00:08:44 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    clothes = {}
+
+    for _ in range(n):
+        name, kind = input().split()
+        if kind in clothes:
+            clothes[kind] += 1
+        else:
+            clothes[kind] = 1
+
+    result = 1
+    for i in clothes.values():
+        result *= (i+1)
+
+    print(result - 1)
