ad244cdbb06efeff812c6532828899e5678e4d9d
Park Hoyeon <hoyeonlove1004@naver.com>
2026-02-10 16:11:06 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 1684.50 ms, Memory: 745 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    arr = []
+    if a <= b:
+        for i in range(a,b+1):
+            arr.append(i)
+    elif a>b:
+        for i in range(b,a+1):
+            arr.append(i)
+    
+    return sum(arr)
