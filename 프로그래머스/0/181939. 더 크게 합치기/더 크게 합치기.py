8b2f4273d873b682386826de6be04fd7b5644b63
Park Hoyeon <hoyeonlove1004@naver.com>
2026-02-13 21:33:22 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    answer1 = int(str(a) + str(b))
+    answer2 = int(str(b) + str(a))
+    
+    if answer1 >= answer2:
+        return answer1
+    else:
+        return answer2
