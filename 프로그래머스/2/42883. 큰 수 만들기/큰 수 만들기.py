e828661737a1e57df3a2c7c5a8261f6f10a644af
Park Hoyeon <hoyeonlove1004@naver.com>
2026-02-20 13:08:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 91.50 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    stack = []
+    
+    for i in number:
+        while k > 0 and stack and stack[-1] < i:
+            stack.pop()
+            k -= 1
+        stack.append(i)
+    
+    if k > 0:
+        stack = stack[:-k]
+    
+    return "".join(stack)
