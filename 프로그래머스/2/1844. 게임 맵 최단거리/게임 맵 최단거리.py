7216b91e259d64a558660b2421de2c0ca5a4e13b
Park Hoyeon <hoyeonlove1004@naver.com>
2026-03-18 21:29:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.31 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(maps):
+    
+    n = len(maps)
+    m = len(maps[0])
+    
+    visited = [[False] * m for _ in range(n)]
+    
+    q = deque()
+    q.append((0,0))
+    visited[0][0] = True
+    
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    while q:
+        x,y = q.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append((nx,ny))
+    
+    if maps[n-1][m-1] == 1:
+        return -1
+    else:
+        return maps[n-1][m-1]
+        
