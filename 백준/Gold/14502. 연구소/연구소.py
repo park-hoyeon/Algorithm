72c9f7025617bde9cd2ec1761515ef95d0d2bc00
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-07 01:49:23 +0900

    [Gold IV] Title: 연구소, Time: 2508 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,14 +1,11 @@
-import sys
-input = sys.stdin.readline
+n,m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+store = [[0]*m for _ in range(n)]
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
-n,m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-temp = [[0]*m for _ in range(n)]
-
-result = 0
+answer = 0
 
 empty = []
 for i in range(n):
@@ -20,38 +17,39 @@ def virus(x,y):
     for i in range(4):
         nx = dx[i] + x
         ny = dy[i] + y
-        if 0<=nx<n and 0<=ny<m and temp[nx][ny] == 0:
-            temp[nx][ny] = 2
+        if 0<=nx<n and 0<=ny<m and store[nx][ny] == 0:
+            store[nx][ny] = 2
             virus(nx,ny)
 
-def get_score():
-    score = 0
+def get_scale():
+    scale = 0
     for i in range(n):
         for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
+            if store[i][j] == 0:
+                scale += 1
+    return scale
 
-def dfs(start, count):
-    global result
+def dfs(start,count):
+    global answer
     if count == 3:
         for i in range(n):
             for j in range(m):
-                temp[i][j] = graph[i][j]
-
+                store[i][j] = graph[i][j]
+        
         for i in range(n):
             for j in range(m):
-                if temp[i][j] == 2:
+                if store[i][j] == 2:
                     virus(i,j)
+        
+        answer = max(answer, get_scale())
+        return 
 
-        result = max(result, get_score())
-        return
-
-    for i in range(start, len(empty)):
+    for i in range(start,len(empty)):
         x,y = empty[i]
         if graph[x][y] == 0:
             graph[x][y] = 1
             dfs(i+1, count+1)
             graph[x][y] = 0
+        
 dfs(0,0)
-print(result)
+print(answer)
