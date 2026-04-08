33cb5f6bc71b9395d4706c8742b79b5bdadcac93
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-09 00:36:19 +0900

    [Silver II] Title: 이동하기, Time: 204 ms, Memory: 124588 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 n,m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0]*m for _ in range(n)]
@@ -8,7 +5,21 @@ dp[0][0] = graph[0][0]
 
 for i in range(n):
     for j in range(m):
-        if i == 0 and j == 0:
+        if i == 0 and j==0:
             continue
-        dp[i][j] = max(dp[i-1][j]+graph[i][j], dp[i][j-1]+graph[i][j])
+
+        up = 0
+        left = 0
+        diag = 0
+
+        if i > 0:
+            up = dp[i-1][j]
+        if j > 0:
+            left = dp[i][j-1]
+        if i>0 and j>0:
+            diag = dp[i-1][j-1]
+
+        dp[i][j] = max(up,left,diag) + graph[i][j]
+
 print(dp[n-1][m-1])
+        
