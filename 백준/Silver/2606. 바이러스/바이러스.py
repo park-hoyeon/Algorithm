65ae724e1ce460faada68c8a4cb58cb5edc53fc8
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-09 20:45:45 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,19 +1,19 @@
-computer = int(input())
+n = int(input())
 pair = int(input())
 
-graph=[[] for _ in range(computer+1)]
-visited = [0]*(computer+1)
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 
 for _ in range(pair):
     a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def find(value):
-    visited[value] = 1
-    for i in graph[value]:
-        if visited[i] == 0:
-            find(i)
-    return
-find(1)
-print(sum(visited)-1)
+def dfs(start):
+    visited[start] = True
+
+    for next in graph[start]:
+        if not visited[next]:
+            dfs(next)
+dfs(1)
+print(visited.count(True) - 1)
