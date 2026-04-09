510ff7f756f95c74df0c91b19455103d17318b1d
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-09 21:15:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 113496 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,36 @@
+from collections import deque
+
 n = int(input())
 graph = [list(map(int, input().strip())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
-def dfs(x,y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return 0
-    if graph[x][y] == 0:
-        return 0
-    
-    graph[x][y] = 0
-    count = 1
+def bfs(x,y):
+    total = 1
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
 
-    for i in range(4):
-        nx = dx[i] + x
-        ny = dy[i] + y
-        count += dfs(nx,ny)
-    return count
+    while q:
+        x,y = q.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny] == 1:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+                total += 1
+    return total
 
 answer = []
-
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
-            answer.append(dfs(i,j))
-
+        if not visited[i][j] and graph[i][j] == 1:
+            answer.append(bfs(i,j))
 answer.sort()
 print(len(answer))
-for i in answer:
-    print(i)
+for x in answer:
+    print(x)
