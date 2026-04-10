fbde9ed091344bb82ed71d4c7a9ffe262c8c8eda
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-10 15:16:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,35 +2,34 @@ from collections import deque
 
 n = int(input())
 graph = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[False]*n for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
 
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
-def bfs(x,y):
-    total = 1
-    q = deque()
-    q.append((x,y))
-    visited[x][y] = True
-
-    while q:
-        x,y = q.popleft()
-
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny] == 1:
-                visited[nx][ny] = True
-                q.append((nx,ny))
-                total += 1
-    return total
-
 answer = []
+
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] and graph[i][j] == 1:
-            answer.append(bfs(i,j))
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            q = deque()
+            q.append((i,j))
+            visited[i][j] = 1
+            count = 1
+
+            while q:
+                x,y = q.popleft()
+                
+                for a in range(4):
+                    nx = dx[a] + x
+                    ny = dy[a] + y
+                    if 0<=nx<n and 0<=ny<n and visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                        visited[nx][ny] = 1
+                        q.append((nx,ny))
+                        count += 1
+            answer.append(count)
 answer.sort()
 print(len(answer))
-for x in answer:
-    print(x)
+
+for i in answer:
+    print(i)
