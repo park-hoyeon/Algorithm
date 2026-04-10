6cf78a6ac8c19d12cfd2d7531ec91fc5b9539e3a
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-10 20:15:31 +0900

    [Silver I] Title: 안전 영역, Time: 700 ms, Memory: 230728 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,36 +1,41 @@
-from collections import deque
-
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-
-def bfs(x,y,h):
-    q =  deque()
-    q.append((x,y))
-    visited[y][x] = True
-
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = dx[i]+x
-            ny = dy[i]+y
-
-            if 0<=nx<n and 0<=ny<n and not visited[ny][nx] and graph[ny][nx] > h:
-                visited[ny][nx] = True
-                q.append((nx, ny))
-
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-max_len = max(map(max, graph))
-result = 0
+graph = [list(map(int,input().split())) for _ in range(n)]
 
-for h in range(0,max_len+1):
-    count = 0
-    visited = [[False] * n for _ in range(n)]
+def dfs(x,y,rain):
+    if x < 0 or x>= n or y < 0 or y >= n:
+        return False
+    
+    if visited[x][y]:
+        return False
+    
+    if graph[x][y] <= rain:
+        return False
+
+    visited[x][y] = True
+
+    dfs(x-1,y,rain)
+    dfs(x+1,y,rain)
+    dfs(x,y-1,rain)
+    dfs(x,y+1,rain)
+    return True
+
+max_h = 0
+for x in range(n):
     for y in range(n):
-        for x in range(n):
-            if graph[y][x] > h and not visited[y][x]:
-                bfs(x,y,h)
-                count+=1
-    result = max(result, count)
-print(result)
+        if graph[x][y] > max_h:
+            max_h = graph[x][y]
+
+answer = 0
+for rain in range(max_h+1):
+    visited = [[False]*n for _ in range(n)]
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if dfs(i,j,rain):
+                count += 1
+    answer = max(answer,count)
+print(answer)
