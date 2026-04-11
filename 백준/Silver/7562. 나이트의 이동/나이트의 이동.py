6834dc09231e8c673991a0dd89b05d010d0610ab
Park Hoyeon <hoyeonlove1004@naver.com>
2026-04-11 18:43:22 +0900

    [Silver I] Title: 나이트의 이동, Time: 268 ms, Memory: 125324 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,28 +3,32 @@ from collections import deque
 t = int(input())
 for _ in range(t):
     l = int(input())
-    now_x, now_y = map(int, input().split())
-    goal_x, goal_y = map(int, input().split())
+    now_x,now_y = map(int, input().split())
+    goal_x,goal_y = map(int,input().split())
 
-    dist = [[-1] * l for _ in range(l)]
+    visited = [[-1]*l for _ in range(l)]
 
     dx = [-2, -2, -1, -1, 1, 1, 2, 2]
     dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
-    q = deque()
-    q.append((now_x, now_y))
-    dist[now_x][now_y] = 0
+    def bfs():
+        q = deque()
+        q.append((now_x,now_y))
+        visited[now_x][now_y] = 0
 
-    while q:
-        x,y = q.popleft()
-        if x == goal_x and y == goal_y:
-            print(dist[x][y])
-            break
+        while q:
+            x,y = q.popleft()
 
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            if x == goal_x and y == goal_y:
+                return visited[x][y]
+            
+            for i in range(8):
+                nx = dx[i] + x
+                ny = dy[i] + y
 
-            if 0 <= nx < l and 0 <= ny < l and dist[nx][ny] == -1:
-                dist[nx][ny] = dist[x][y] + 1
-                q.append((nx,ny))
+                if 0<=nx<l and 0<=ny<l:
+                    if visited[nx][ny] == -1:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append((nx,ny))
+    
+    print(bfs())
