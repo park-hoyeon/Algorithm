5cddc35a3715a135fcbd29dd84df4eb663d4beed
Park Hoyeon <hoyeonlove1004@naver.com>
2026-05-18 23:37:21 +0900

    [level 2] Title: 배달, Time: 0.93 ms, Memory: 9.73 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,22 +1,20 @@
 import heapq
 
-def solution(n, road, k):
+def solution(n, road, K):
+
     INF = int(1e9)
-    
     distance = [INF] * (n+1)
-    
+
     graph = [[] for _ in range(n+1)]
-    for i in range(len(road)):
-        a = road[i][0]
-        b = road[i][1]
-        c = road[i][2]
-        
+    for i in road:
+        a = i[0]
+        b = i[1]
+        c = i[2]
         graph[a].append((b,c))
-        graph[b].append((a, c))
-    
+        graph[b].append((a,c))
+        
     def dijkstra(start):
         q = []
-        
         heapq.heappush(q,(0,start))
         distance[start] = 0
         
@@ -25,15 +23,17 @@ def solution(n, road, k):
             
             if distance[now] < dist:
                 continue
+            
             for i in graph[now]:
                 cost = dist + i[1]
                 if cost < distance[i[0]]:
                     distance[i[0]] = cost
                     heapq.heappush(q,(cost,i[0]))
+    
     dijkstra(1)
     
     answer = 0
     for i in distance:
-        if i <= k:
+        if i <= K:
             answer += 1
     return answer
